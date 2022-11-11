c6486d5be9e02dfc977499fc35ef620202b48edd
Fidelio <fiocheah08@gmail.com>
2022-11-10 22:48:44 -0600

    Update The Schema

--- 3380project.sql
+++ 3380project.sql
@@ -1,90 +1,64 @@
-drop table if exists season;
-drop table if exists play;
-drop table if exists players;
-drop table if exists sign;
+
+
+use cs3380;
+
 drop table if exists team;
-drop table if exists conference;
-drop table if exists standing;
-drop table if exists leaderboard;
-drop table if exists generate;
-drop table if exists gameData;
-drop table if exists homedata;
-drop table if exists awaydata;
-drop table if exists manager;
-drop table if exists owner;
-drop table if exists coach;
 drop table if exists employee;
 drop table if exists city;
-drop table if exists complete;
+drop table if exists conference;
+drop table if exists generate;
+drop table if exists gameData;
+drop table if exists standing;
+drop table if exists leaderboard;
+drop table if exists compete;
+drop table if exists season;
+drop table if exists signed;
+drop table if exists players;
+drop table if exists play;
 
 
-create table season(
-   seasonID integer primary key,
-   season_year integer not null
-);
-create table player(
-   playerID integer primary key,
-   playerName text not null
-);
-create table play(
-   playerID integer REFERENCES player(playerID)
-);
-create table sign(
-   playerID integer REFERENCES player(playerID),
-   teamID integer REFERENCES team(teamID)
-);
 create table team(
    teamID integer primary key,
    abbreviation text not null,
    nickname text not null,
    yearFounded integer not null,
-   arena text not null,
+   arenaName text not null,
    arenaCapacity integer,
    dleagueAffiliation text not null
-);
-create table conference(
-   conferenceID integer primary key,
-   conference text not null
-);
-
-create table manager(
-   managerID integer primary key,
-   generalManager text not null
-);
-
-create table owner(
-   ownerID integer primary key,
-   owner text not null
-);
-
-create table coach(
-coachID integer primary key,
-headCoach text not null
+   FOREIGN key ("cityID") REFERENCES "city" ("cid")
+   FOREIGN key ("conferenceID") REFERENCES "conference" ("conferenceID")
 );
 
 create table employee(
-   /*union:employee -> owner, manager, coach*/
-   employeeID integer primary key,
-   name text not null
+   employeeTeam INTEGER PRIMARY key IDENTITY (1,1),
+   coach text not null,
+   manager text not null,
+   owner text not null
+   FOREIGN key ("teamID") REFERENCES "team" ("teamID")
 );
 
 create table city(
-   cityID integer primary key,
+   cityID integer primary key IDENTITY (1,1),
    cityName text not null
 );
 
-create table complete(
-   seasonID integer REFERENCES season(seasonID),
-   teamID integer REFERENCES team(teamID)
+create table conference(
+   conferenceID integer primary key IDENTITY(1,1),
+   conference text not null
+);
+
+create table generate(
+   "gameID" INTEGER,
+   "teamID" INTEGER,
+   PRIMARY KEY ("gameID","teamID"),
+   FOREIGN KEY ("gameID") REFERENCES "gameData"("gameID"),
+   FOREIGN KEY ("teamID") REFERENCES "team"("teamID")
 );
 
 create table gameData(
    gameID integer primary key,
-   gamedate text not null
-);
-
-create table homeData(
-   gameDate text REFERENCES gameData(gameDate),
+   gameDate text not null,
+   
    homeTeamID integer primary key,
    ptsHome integer not null,
    fgPtsHome float not null,
@@ -93,11 +67,8 @@ create table homeData(
    fg3PctHome float not null,
    astHome integer not null,
    rebHome integer not null,
-   homeTeamWins integer not null
-);
+   homeTeamWins integer not null,
 
-create table awayData(
-   gameDate text REFERENCES gameData(gameDate),
    awayTeamID integer primary key,
    ptsAway integer not null,
    fgPtsAway float not null,
@@ -109,23 +80,61 @@ create table awayData(
    awayTeamWins integer not null
 );
 
-create table generate(
-   gameID integer REFERENCES gameData(gameID),
-   teamID integer REFERENCES team(teamID)
+create table standings(
+   "teamID" INTEGER,
+   "standingsDate" INTEGER,
+   PRIMARY key ("teamID", "standingsDate"),
+   FOREIGN KEY ("teamID") REFERENCES "team"("teamID"),
+   FOREIGN KEY ("standingsDate") REFERENCES "leaderboard"("standingsDate")
 );
 
 create table leaderboard(
-   standingDate integer primary key,
-   gamePlayed integer not null,
-   gameWon integer not null,
-   gameLost integer not null,
+   standingsDate integer primary key,
+   gamesPlayed integer not null,
+   gamesWon integer not null,
+   gamesLost integer not null,
    winPercent integer not null,
    homeRecord integer not null,
    awayRecord integer not null
 );
 
-create table standing(
-   teamID integer REFERENCES team(teamID),
-   standingDate integer REFERENCES leaderboard(standingDate)
+create table compete(
+   "seasonID" INTEGER,
+   "teamID" INTEGER,
+   PRIMARY KEY ("seasonID", "teamID"),
+   FOREIGN KEY ("seasonID") REFERENCES "season"("seasonID"),
+   FOREIGN KEY ("teamID") REFERENCES "team"("teamID")
 );
+
+create table season(
+   seasonID integer primary key,
+   season_year integer not null
+);
+
+create table signed(
+   "playerID" INTEGER,
+   "teamID" INTEGER,
+   PRIMARY KEY ("playerID", "teamID"),
+   FOREIGN KEY ("playerID") REFERENCES "player"("playerID"),
+   FOREIGN KEY ("teamID") REFERENCES "team"("teamID")
+);
+
+create table player(
+   playerID integer primary key,
+   playerName text not null
+);
+
+create table play(
+   "playerID" INTEGER,
+   "seasonID" INTEGER,
+   PRIMARY KEY ("playerID", "seasonID"),
+   FOREIGN KEY ("playerID") REFERENCES "player"("playerID"),
+   FOREIGN key ("seasonID") REFERENCES "season"("seasonID")
+);
+
+
+
+
+
+
 
