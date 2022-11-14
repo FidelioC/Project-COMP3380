7dea33f09b211f125fbf3f279d6c80e0d9791750
Fidelio <fiocheah08@gmail.com>
2022-11-14 16:17:10 -0600

    Update Order Drop Table

--- Part2-Project/create-table.sql
+++ Part2-Project/create-table.sql
@@ -1,17 +1,18 @@
 use cs3380;
 
+drop table if exists standings;
+drop table if exists play;
 drop table if exists generate;
+drop table if exists compete;
+drop table if exists signed;
 drop table if exists team;
 drop table if exists city;
 drop table if exists conference;
 drop table if exists gameData;
-drop table if exists standings;
 drop table if exists leaderboard;
-drop table if exists compete;
 drop table if exists season;
-drop table if exists signed;
 drop table if exists player;
-drop table if exists play;
+
 
 create table city(
    cityID integer primary key IDENTITY (1,1),
