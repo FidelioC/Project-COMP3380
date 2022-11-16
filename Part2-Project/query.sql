c258139e7a6da8440b1db449ebc1e8719a90f73f
Fidelio <fiocheah08@gmail.com>
2022-11-16 16:50:15 -0600

    Make 7 Queries

--- Part2-Project/query.sql
+++ Part2-Project/query.sql
@@ -1,7 +1,10 @@
-/*search the team name from the city*/
-Select nickname from team
+/* City that has more than one team */
+SELECT cityName
+from team
 join city on city.cityID = team.cityID
-where cityName like 'Los Angeles' /*city name is the input*/
+group by cityName
+having count(cityName) > 1
+
 
 /*Show the game ID, and home team where home team get 135+ points*/
 SELECT gameID, nickname
@@ -10,6 +13,54 @@ left join team on team.teamID = gameData.homeTeamID
 where ptsHome > 135; 
 
 /*the highest score each team has made at home*/
-SELECT distinct teamID
-from team;
+with allTeam as(
+  SELECT team.teamID, teamName, gameData.ptsHome, gameData.gameID
+  from team
+  join generate on team.teamID = generate.teamID
+  join gameData on generate.gameID = gameData.gameID
+  where team.teamID = gameData.homeTeamID
+)
+select teamID, teamName, max(ptsHome) as HighestPoint
+from allTeam
+group by teamID, teamName
+order by teamID desc;
+
+/*players stay in one team*/
+select player.playerID, playerName, count(team.teamID)
+from player
+join signed on player.playerID = signed.playerID
+join team on team.teamID = signed.teamID
+group by player.playerID, playerName
+having count(team.teamID) = 1
+
+/* A player play on which seasons and play on what team for each of that season. 
+Search by the player name*/
+SELECT player.playerName, teamName, compete.season_year from player
+join season on season.playerID = player.playerID
+join compete on player.playerID = compete.playerID 
+      and season.season_year = compete.season_year
+join team on team.teamID = compete.teamID
+where playerName = 'Kobe Bryant';
+
+
+/*teams on each conference*/
+SELECT teamName, conference
+from team
+join conference on conference.conferenceID = team.conferenceID
+ORDER by conference
+
+/*The roster of a team on a specific season*/
+SELECT teamName, player.playerName, season_year
+from compete
+join team on team.teamID = compete.teamID
+join player on compete.playerID = player.playerID
+where season_year = 2019 and teamName = 'Portland'
+order by teamName
+
+
+
+
+
+
+
 
